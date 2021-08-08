2895809acd914f794dd1a38e38859691d149235c
Siddarth Reddy <siddarthreddy4923@gmail.com>
2021-08-08 19:27:14 +0530

    added keymaps

--- clean.py
+++ clean.py
@@ -12,6 +12,13 @@ start = "end-of-line"
 start1 = "endif"
 end = "~/.bash_logout"
 text = ""
+keymaps = {
+	r"\33[A":"[uparr]\n",
+	r"\33[B":"[downarr]\n",
+	r"\33[C":"[rightarr]",
+	r"\33[D":"[leftarr]",
+	r"\177":"[backspace]",
+}
 with open(filepath) as f:
 	lines = f.readlines()
 	found = 0
@@ -24,6 +31,10 @@ with open(filepath) as f:
 			found = 1
 		elif start and start1 not in line and found == 1:
 			line = line.rstrip()
+			line = line.replace(r'\r','\n')
+			line = line.replace(r'\t','[tab]\n')
 			text += line
+for key in keymaps:
+	text = text.replace(key,keymaps[key])
 with open(filename +"-f.log","w") as f:
 	f.write(text)
