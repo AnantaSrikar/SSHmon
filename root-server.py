b2d7c6a09706811a32e2abf00037b214117dfef1
Siddarth Reddy <siddarthreddy4923@gmail.com>
2021-08-08 20:43:44 +0530

    added start and end messages

--- root-server.py
+++ root-server.py
@@ -11,6 +11,12 @@ def home():
 def newShell():
 	data = request.get_json()
 
-	system(f"./log-user.sh {data['user']} {data['PID']}")
+	print(data)
+	print(f"Started logging for {data['user']}")
+	system(f"./log-user.sh {data['user']} {str(data['PID'])}")
+	print(f"Logging ended for {data['user']} with PID {str(data['PID'])}")
 
 	return ""
+
+if __name__ == "__main__":
+	app.run(debug=True, port=8080)
