a0a39fb5b0224088d65254fe33f616919c0e8396
Siddarth Reddy <siddarthreddy4923@gmail.com>
2021-08-08 20:43:08 +0530

    changed logging nomenclature

--- log-user.sh
+++ log-user.sh
@@ -10,7 +10,7 @@ UPID=$2
 if [ ! -d "/tmp/$UNAME" ]; then
 	mkdir /tmp/$UNAME
 fi
-filename=$(date +"%Y%m%d-%T")
+filename=$(date +%Y-%m-%d_%H-%M-%S)
 strace -s 16384 -p $UPID -e read 2>&1 | stdbuf -oL grep -oP '"\K[^"\047]+(?=["\047])' >> /tmp/$UNAME/$filename.log
 python3 clean.py --file $filename.log --user $UNAME
 cp $filename.log-f.log /tmp/$UNAME/
