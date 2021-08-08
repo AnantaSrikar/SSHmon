57798cb1a2d2ca05ba7a070b0ae2ad13128e8493
Ananta Srikar <47946629+AnantaSrikar@users.noreply.github.com>
2021-08-08 22:07:27 +0530

    Removed code used for debugging

--- root-server.py
+++ root-server.py
@@ -11,12 +11,6 @@ def home():
 def newShell():
 	data = request.get_json()
 
-	print(data)
-	print(f"Started logging for {data['user']}")
 	system(f"./log-user.sh {data['user']} {str(data['PID'])}")
-	print(f"Logging ended for {data['user']} with PID {str(data['PID'])}")
 
 	return ""
-
-if __name__ == "__main__":
-	app.run(debug=True, port=8080)
